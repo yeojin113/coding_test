7296398b0f5f1b4a371201be06e81229f0612216
yeowls <yeowls113@naver.com>
2026-06-25 14:11:20 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.03 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
@@ -1,23 +1,35 @@
+"""
 def solution(array):  
     mode = {}
-    n = 0
     
     for num in array:
         if num in mode:
             mode[num] += 1
         else:
             mode[num] = 1
-    
-    for num in array :
-        mode[num] += 1
         
     max_num = max(mode.values())
     
-    list = []
+    modes = []
     for key, value in mode.items() :
         if value == max_num :
-            list.append(key)
-    if len(list) >= 2 :
+            modes.append(key)
+            
+    if len(modes) >= 2 :
         return -1
     else :
-        return list[0]
+        return modes[0]
+"""
+
+# 답
+from collections import Counter
+
+def solution(array):
+    counter = Counter(array)
+
+    max_count = max(counter.values())
+
+    if list(counter.values()).count(max_count) > 1:
+        return -1
+
+    return max(counter, key=counter.get)
