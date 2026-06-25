4e49ddc112ee5a9a2de3575ebd709aa29cadc1cf
yeowls <yeowls113@naver.com>
2026-06-25 11:45:40 +0900

    Update 중앙값 구하기.py
    Refactor solution function to remove unnecessary sorting logic.

--- 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.py
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.py
@@ -1,10 +1,12 @@
-""" def solution(array):
+""" 
+def solution(array):
     for i in range(len(array)) :
         for j in range(i+1, len(array)) : 
             if array[i] > array[j] :
                 array[i], array[j] = array[j], array[i]
     
-    return array[len(array) // 2] """
+    return array[len(array) // 2] 
+"""
 
 # 쉽게는
 def solution(array):
