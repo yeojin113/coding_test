929ca3c473d853f6ecd7a4c6ca4d517d433f2757
yeowls <yeowls113@naver.com>
2026-06-25 11:05:15 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.04 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.py
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.py
@@ -1,7 +1,3 @@
-def solution(numbers):
-    l = len(numbers)
-    
-    for i in range(0,l) :
-        numbers[i] *= 2
-        
-    return numbers
+def solution(numbers) : 
+    # 기존 리스트를 기반으로 새로운 리스트를 간결하고 직관적으로       생성하는 문법, 리스트 안에 코드를 작성하는 게 특징     
+    return [i*2 for i in numbers]
