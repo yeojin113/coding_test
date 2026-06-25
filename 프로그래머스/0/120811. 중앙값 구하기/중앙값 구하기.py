9029cf86db4b6a02ca2f458d6cf3e15d225038db
yeowls <yeowls113@naver.com>
2026-06-25 11:45:05 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.01 ms, Memory: 11.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.py
@@ -0,0 +1,11 @@
+""" def solution(array):
+    for i in range(len(array)) :
+        for j in range(i+1, len(array)) : 
+            if array[i] > array[j] :
+                array[i], array[j] = array[j], array[i]
+    
+    return array[len(array) // 2] """
+
+# 쉽게는
+def solution(array):
+    return sorted(array)[len(array) // 2]
